6cd8a2bf25de1f134ffd9cad020b912a7bcd2d8b
Tomiță Mircea-Ștefan <136245979+Mircea-Stefan@users.noreply.github.com>
2023-12-10 18:37:52 +0200

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+inline bool isPrime(unsigned long long int x) {
+    if (x < 2)
+        return false;
+    if (x == 2)
+        return true;
+    if ((x & 1) == 0)
+        return false;
+    for (unsigned long long int d = 3; d * d <= x; d += 2)
+        if (x % d == 0)
+            return false;
+    return true;
+}
+int main() {
+    return 0;
+}
