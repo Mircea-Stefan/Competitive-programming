86ae9eb3d03af5cb618a153d90631f1a8febf4e3
Tomiță Mircea-Ștefan <136245979+Mircea-Stefan@users.noreply.github.com>
2023-12-10 18:43:17 +0200

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+unsigned long long int lg_pow(unsigned long long int x, char pow) {
+    unsigned long long int ans = 1;
+    while (pow != 0) {
+        if (pow & 1)
+            ans *= x;
+        x *= x;
+        pow >>= 1;
+    }
+    return ans;
+}
+int main() {
+    return 0;
+}
