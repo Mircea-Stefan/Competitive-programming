9a2714fbecda6f2d2a740a9dcb803211cf260aec
Tomiță Mircea-Ștefan <136245979+Mircea-Stefan@users.noreply.github.com>
2023-12-10 17:02:45 +0200

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <bitset>
+#include <vector>
+const unsigned long long int NMAX = 1e8;
+unsigned long long int i, j;
+std :: bitset <(NMAX >> 1) + 1> sieve;
+std :: vector <unsigned long long int> primes;
+int main() {
+    for (i = 1; ((i * (i + 1)) << 1) <= NMAX; ++ i)
+        if (sieve[i] == 0)
+            for (j = (i * (i + 1)) << 1; (j << 1) < NMAX; j += (i << 1) + 1)
+                sieve[j] = 1;
+    primes.emplace_back(2);
+    for (i = 3; i <= NMAX; i += 2)
+        if (sieve[i >> 1] == 1)
+            primes.emplace_back(i);
+    return 0;
+}
