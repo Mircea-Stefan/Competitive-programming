59e27ae375997c70250868742084494ae0c59194
Tomiță Mircea-Ștefan <136245979+Mircea-Stefan@users.noreply.github.com>
2023-12-10 17:44:12 +0200

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <bitset>
+#include <vector>
+static const long long int NMAX = 1e8;
+long long int i, j, aux;
+std :: bitset <NMAX + 1> sieve;
+std :: vector <long long int> primes;
+int main() {
+    sieve[2] = sieve[3] = 1;
+    for (i = 1; i * i <= NMAX; ++ i)
+        for (j = 1; j * j <= NMAX; ++ j) {
+            aux = ((i * i) << 2) + j * j;
+            if (aux <= NMAX and (aux % 12 == 1 or aux % 12 == 5))
+                sieve[aux] = sieve[aux] ^ 1;
+            aux -= i * i;
+            if (aux <= NMAX and aux % 12 == 7)
+                sieve[aux] = sieve[aux] ^ 1;
+            aux -= (j * j) << 1;
+            if (i > j and aux <= NMAX and aux % 12 == 11)
+                sieve[aux] = sieve[aux] ^ 1;
+        }
+    for (i = 5; i * i <= NMAX; i += 2)
+        if (sieve[i] == 1)
+            for (j = i * i; j <= NMAX; j += i * i)
+                sieve[j] = 0;
+    primes.emplace_back(2);
+    for (i = 3; i <= NMAX; i += 2)
+        if (sieve[i] == 1)
+            primes.emplace_back(i);
+    return 0;
+}
