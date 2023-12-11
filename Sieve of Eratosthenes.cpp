6546d67f3f13ecd12c389a8f274dcafeb251a3d5
Tomiță Mircea-Ștefan <136245979+Mircea-Stefan@users.noreply.github.com>
2023-12-11 08:16:04 +0200

    Update Sieve of Eratosthenes.cpp

--- Sieve of Eratosthenes.cpp
+++ Sieve of Eratosthenes.cpp
@@ -6,12 +6,24 @@ unsigned long long int i, j;
 std :: bitset <(NMAX >> 1) + 1> sieve;
 std :: vector <unsigned long long int> primes;
 int main() {
-    for (i = 3; i * i <= NMAX; i += 2)
-        if (sieve[i >> 1] == 0)
+    primes.emplace_back(2);
+    primes.emplace_back(3);
+    for (j = 9; j <= NMAX; j += 6)
+        sieve[j >> 1] = 1;
+    for (i = 5; i * i <= NMAX; i += 4) {
+        if (sieve[i >> 1] == 0) {
+            primes.emplace_back(i);
             for (j = i * i; j <= NMAX; j += (i << 1))
                 sieve[j >> 1] = 1;
-    primes.emplace_back(2);
-    for (i = 3; i <= NMAX; i += 2)
+        }
+        i += 2;
+        if (sieve[i >> 1] == 0) {
+            primes.emplace_back(i);
+            for (j = i * i; j <= NMAX; j += (i << 1))
+                sieve[j >> 1] = 1;
+        }
+    }
+    for (i = primes.back() + 2; i <= NMAX; i += 2)
         if (sieve[i >> 1] == 0)
             primes.emplace_back(i);
     return 0;
