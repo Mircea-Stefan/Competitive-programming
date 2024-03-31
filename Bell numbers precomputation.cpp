e240743895eb2e9c1a0caafd8fb66f7cf9e76d55
Tomiță Mircea-Ștefan <136245979+Mircea-Stefan@users.noreply.github.com>
2024-03-31 18:37:04 +0300

    Update and rename Bell numbers.cpp to Bell numbers precomputation.cpp

---
+++ Bell numbers precomputation.cpp
@@ -0,0 +1,17 @@
+static const uint16_t MaxBell = 26;
+uint64_t bell[MaxBell + 1];
+void precomputeBell() {
+	uint64_t aux[MaxBell + 1], prev, temp;
+	aux[0] = bell[0] = 1;
+	uint16_t i, j;
+	for (i = 1; i <= MaxBell; ++ i) {
+		prev = aux[0];
+		aux[0] = aux[i - 1];
+		for (j = 1; j <= i; ++ j) {
+			temp = aux[j];
+			aux[j] = prev + aux[j - 1];
+			prev = temp;
+		}
+		bell[i] = aux[0];
+	}
+}
