2805cadf0b3d0976529569d935b60863701156fb
Tomiță Mircea-Ștefan <136245979+Mircea-Stefan@users.noreply.github.com>
2024-03-31 19:19:51 +0300

    Update Extended euclidean algorithm.cpp

--- Extended euclidean algorithm.cpp
+++ Extended euclidean algorithm.cpp
@@ -1,10 +1,10 @@
-int64_t ext_gcd(int64_t a, int64_t b, int64_t &x, int64_t &y) {
+int64_t extGCD(int64_t a, int64_t b, int64_t &x, int64_t &y) {
     if (a == 0) {
         x = 0;
         y = 1;
         return b;
     }
-    int64_t x1, y1, gcd = ext_gcd(b % a, a, x1, y1);
+    int64_t x1, y1, gcd = extGCD(b % a, a, x1, y1);
     x = y1 - b / a * x1;
     y = x1;
     return gcd;
