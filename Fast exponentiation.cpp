8b1aad6abebd64400c0ee78a491f5d19a6bac8f9
Tomiță Mircea-Ștefan <136245979+Mircea-Stefan@users.noreply.github.com>
2024-03-31 19:22:36 +0300

    Update Fast exponentiation.cpp

--- Fast exponentiation.cpp
+++ Fast exponentiation.cpp
@@ -1,7 +1,7 @@
-int64_t lg_pow(int64_t x, uint8_t pow) {
-    int64_t ans = 1;
+int64_t fastExp(int64_t x, uint16_t pow) {
+	int64_t ans = 1;
     while (pow != 0) {
-        if (pow & 1)
+        if ((pow & 1) == 1)
             ans *= x;
         x *= x;
         pow >>= 1;
