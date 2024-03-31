b068f93ddf51857854f53a7a7d5b551f87cdaccc
Tomiță Mircea-Ștefan <136245979+Mircea-Stefan@users.noreply.github.com>
2024-03-31 20:25:31 +0300

    Update and rename Stirling numbers of the second kind.cpp to Stirling numbers of the second kind precomputation.cpp

--- Stirling numbers of the second kind precomputation.cpp
+++ Stirling numbers of the second kind precomputation.cpp
@@ -1,16 +1,19 @@
-#include <iostream>
-static const uint32_t NMAX = 20, RMAX = 20;
-uint64_t fact[NMAX + 1], stirling[NMAX + 1][RMAX + 1], i, j;
-int main() {
+const uint16_t NMAX = 20, RMAX = 20;
+uint64_t fact[NMAX + 1], stirling[NMAX + 1][RMAX + 1];
+void precomputeFactorial() {
     fact[0] = fact[1] = 1;
-    for (i = 2; i <= NMAX; ++ i)
+    for (uint64_t i = 2; i <= NMAX; ++ i)
         fact[i] = fact[i - 1] * i;
+}
+void precomputeStirling() {
+	precomputeFactorial();
+	uint16_t i;
     for (i = 0; i <= NMAX; ++ i)
         stirling[i][i] = 1;
     for (i = 1; i <= RMAX; ++ i)
         stirling[1][i] = fact[i - 1];
+	uint64_t j;
     for (i = 2; i <= NMAX; ++ i)
         for (j = 2; j <= RMAX; ++ j)
             stirling[i][j] = stirling[i - 1][j - 1] + (j - 1) * stirling[i][j - 1];
-    return 0;
 }
